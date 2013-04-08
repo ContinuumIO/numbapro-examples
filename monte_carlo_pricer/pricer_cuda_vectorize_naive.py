3c14c3334de4d0b45da19de11a5ef937826b2db7
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-04-08 17:29:15 -0500

    Add cuda vectorize naive version

---
+++ monte_carlo_pricer/pricer_cuda_vectorize_naive.py
@@ -0,0 +1,38 @@
+import numpy as np
+import math
+
+import numbapro
+from numbapro import cuda, vectorize
+from numbapro.cudalib import curand
+
+@vectorize(['f8(f8, f8, f8, f8, f8)'], target='gpu')
+def step(last, dt, c0, c1, noise):
+    return last * math.exp(c0 * dt + c1 * noise)
+
+def monte_carlo_pricer(paths, dt, interest, volatility):
+    n = paths.shape[0]
+
+    blksz = cuda.get_current_device().MAX_THREADS_PER_BLOCK
+    gridsz = int(math.ceil(float(n) / blksz))
+
+    # Instantiate cuRAND PRNG
+    prng = curand.PRNG(curand.PRNG.MRG32K3A)
+
+    # Allocate device side array
+    d_normdist = cuda.device_array(n, dtype=np.double)
+    
+    c0 = interest - 0.5 * volatility ** 2
+    c1 = volatility * math.sqrt(dt)
+
+    # Simulation loop
+    d_last = cuda.to_device(paths[:, 0])
+    for j in range(1, paths.shape[1]):
+        prng.normal(d_normdist, mean=0, sigma=1)
+        d_paths = cuda.to_device(paths[:, j])
+        step(d_last, dt, c0, c1, d_normdist, out=d_paths)
+        d_paths.copy_to_host(paths[:, j])
+        d_last = d_paths
+
+if __name__ == '__main__':
+    from driver import driver
+    driver(monte_carlo_pricer)
