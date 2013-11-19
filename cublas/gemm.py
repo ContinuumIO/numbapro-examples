37d0294ce1e8a8528b1db510d02db9b9fa36a92b
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-11-19 11:16:38 -0600

    Expand gemm example to showcase transposing

--- cublas/gemm.py
+++ cublas/gemm.py
@@ -10,12 +10,17 @@ import numbapro.cudalib.cublas as cublas
 import numpy as np
 from timeit import default_timer as timer
 
-def main():
+N = 128     # no. of rows/cols
+
+def gemm_v1():
+    '''
+    Note that all arrays are in Fortran order.
+    '''
+    print("Version 1".center(80, '='))
     # Prepare arrays for input
-    N = 128
     A = np.array(np.arange(N ** 2, dtype=np.float32).reshape(N, N), order='F')
     B = np.array(np.arange(N) + 10, dtype=A.dtype, order='F')
-    D = np.zeros_like(A)
+    D = np.zeros_like(A, order='F')
 
     # NumPy
     start = timer()
@@ -24,17 +29,54 @@ def main():
     print("Numpy took %f seconds" % numpy_time)
 
     # cuBLAS
-    blas = cublas.api.Blas()
+    blas = cublas.Blas()
     
     start = timer()
     blas.gemm('N', 'N', N, N, N, 1.0, A, np.diag(B), 1.0, D)
-
     cuda_time = timer() - start
 
     print("CUBLAS took %f seconds" % cuda_time)
     diff = np.abs(D - E)
     print("Maximum error %f" % np.max(diff))
 
+
+def gemm_v2():
+    """
+    Let GEMM transpose the input matrices so that they can be in C order,
+    originally.  Note that the output matrix is still in Fortran array.
+    The string arguments in gemm tells it to apply transformation on the input
+    matrices.
+    
+    See argument description in:
+        http://docs.continuum.io/numbapro/cudalib.html#blas-level-2
+    """
+    print("Version 2".center(80, '='))
+    # Prepare arrays for input
+    A = np.array(np.arange(N ** 2, dtype=np.float32).reshape(N, N))
+    B = np.array(np.arange(N) + 10, dtype=A.dtype)
+    D = np.zeros_like(A, order='F')
+
+    # NumPy
+    start = timer()
+    E = np.dot(A, np.diag(B))
+    numpy_time = timer() - start
+    print("Numpy took %f seconds" % numpy_time)
+
+    # cuBLAS
+    blas = cublas.Blas()
+    
+    start = timer()
+    blas.gemm('T', 'T', N, N, N, 1.0, A, np.diag(B), 1.0, D)
+    cuda_time = timer() - start
+
+    print("CUBLAS took %f seconds" % cuda_time)
+    diff = np.abs(D - E)
+    print("Maximum error %f" % np.max(diff))
+
+
+def main():
+    gemm_v1()
+    gemm_v2()
 
 if __name__ == '__main__':
    main()
